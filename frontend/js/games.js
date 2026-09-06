bf8ae655eb82f3f8d6e19b9fbaff83a37be1ea60
minsukim657 <295185074+minsukim657@users.noreply.github.com>
2026-09-06 21:30:26 +0900

    Rename time match game in admin UI

--- frontend/js/games.js
+++ frontend/js/games.js
@@ -1,7 +1,7 @@
 export const GAME_TYPES = [
   { id: 'BASKETBALL', name: '농구게임' },
   { id: 'PINBALL', name: '핀볼 관전 게임' },
-  { id: 'TIME_MATCH', name: '시간 맞추기' },
+  { id: 'TIME_MATCH', name: '스톱워치 게임' },
   { id: 'OX_QUIZ', name: 'OX 퀴즈' },
   { id: 'RPS', name: '가위바위보' },
   { id: 'WORD_GUESS', name: '제시어 맞히기' },
